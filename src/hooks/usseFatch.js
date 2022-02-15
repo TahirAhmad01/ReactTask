f12cfa0bc7071d2c4ede61d0c1e9048db510e770
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-02-16 02:31:58 +0600

    package update

--- src/hooks/usseFatch.js
+++ src/hooks/usseFatch.js
@@ -1,28 +1,28 @@
 import { useEffect, useState } from "react";
 
 export default function useFetch() {
-    const [error, setError] = useState(null);
-    const [loading, setIsLoaded] = useState(false);
-    const [Api, setApi] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setIsLoaded] = useState(false);
+  const [Api, setApi] = useState([]);
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-            .then((res) => res.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setApi(result);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            );
-    }, []);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setApi(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
 
-    return {
-        loading,
-        error,
-        Api,
-    };
+  return {
+    loading,
+    error,
+    Api,
+  };
 }
